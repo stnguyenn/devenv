68dc362ef9f92f759200e76388686db5e08a9c37
stnguyenn <42306483+stnguyenn@users.noreply.github.com>
2022-12-16 07:29:03 +0000

    Declaring a Winner

--- my-app/src/Game.js
+++ my-app/src/Game.js
@@ -16,8 +16,31 @@ const Board = (props) => {
     const [squares, setSquares] = useState(Array(9).fill(null));
     const [xIsNext, setXIsNext] = useState(true);
 
+    const calculateWinner = (ss) => {
+        const lines = [
+            [0, 1, 2],
+            [3, 4, 5],
+            [6, 7, 8],
+            [0, 3, 6],
+            [1, 4, 7],
+            [2, 5, 8],
+            [0, 4, 8],
+            [2, 4, 6],
+        ];
+        for (let i = 0; i < lines.length; i++) {
+            const [a, b, c] = lines[i];
+            if (ss[a] && ss[a] === ss[b] && ss[a] === ss[c]) {
+                return ss[a];
+            }
+        }
+        return null;
+    }
+
     const handleClick = (i) => {
         const ss = squares.slice();
+        if (calculateWinner(ss) || ss[i]) {
+            return;
+        }
         ss[i] = xIsNext ? 'X' : 'O';
         setSquares(ss);
         setXIsNext(!xIsNext);
@@ -27,8 +50,14 @@ const Board = (props) => {
         return <Square value={squares[i]} onClick={() => handleClick(i)} />;
     }
 
-    const status = 'Next player: ' + (xIsNext ? 'X' : 'O');
-    
+    const winner = calculateWinner(squares);
+    let status;
+    if (winner) {
+        status = 'Winner: ' + winner;
+    } else {
+        status = 'Next player: ' + (xIsNext ? 'X' : 'O');
+    }
+
     return (
         <div>
             <div className="status">{status}</div>
