a6b5395f146a6bc42711e2af69cce4fd749d8e54
steevpara <stephen.nguyen@cbsinteractive.com>
2022-12-17 02:08:54 -0600

    history

--- my-app/src/Game.js
+++ my-app/src/Game.js
@@ -13,54 +13,18 @@ const Square = (props) => {
 
 const Board = (props) => {
 
-    const [squares, setSquares] = useState(Array(9).fill(null));
-    const [xIsNext, setXIsNext] = useState(true);
-
-    const calculateWinner = (ss) => {
-        const lines = [
-            [0, 1, 2],
-            [3, 4, 5],
-            [6, 7, 8],
-            [0, 3, 6],
-            [1, 4, 7],
-            [2, 5, 8],
-            [0, 4, 8],
-            [2, 4, 6],
-        ];
-        for (let i = 0; i < lines.length; i++) {
-            const [a, b, c] = lines[i];
-            if (ss[a] && ss[a] === ss[b] && ss[a] === ss[c]) {
-                return ss[a];
-            }
-        }
-        return null;
-    }
-
-    const handleClick = (i) => {
-        const ss = squares.slice();
-        if (calculateWinner(ss) || ss[i]) {
-            return;
-        }
-        ss[i] = xIsNext ? 'X' : 'O';
-        setSquares(ss);
-        setXIsNext(!xIsNext);
-    }
-
     const renderSquare = (i) => {
-        return <Square value={squares[i]} onClick={() => handleClick(i)} />;
-    }
+        return (
+            <Square
+                value={props.squares[i]}
+                onClick={() => props.onClick(i)}
+            />
+        );
 
-    const winner = calculateWinner(squares);
-    let status;
-    if (winner) {
-        status = 'Winner: ' + winner;
-    } else {
-        status = 'Next player: ' + (xIsNext ? 'X' : 'O');
     }
 
     return (
         <div>
-            <div className="status">{status}</div>
             <div className="board-row">
                 {renderSquare(0)}
                 {renderSquare(1)}
@@ -80,15 +44,87 @@ const Board = (props) => {
     );
 }
 
+const calculateWinner = (ss) => {
+    const lines = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6],
+    ];
+    for (let i = 0; i < lines.length; i++) {
+        const [a, b, c] = lines[i];
+        if (ss[a] && ss[a] === ss[b] && ss[a] === ss[c]) {
+            return ss[a];
+        }
+    }
+    return null;
+}
+
 const Game = (props) => {
+
+    const [history, setHistory] = useState([{
+        squares: Array(9).fill(null),
+    }]);
+    const [xIsNext, setXIsNext] = useState(true);
+    const [stepNumber, setStepNumber] = useState(0);
+
+    const jumpTo = (step) => {
+        setStepNumber(step);
+        setXIsNext((step % 2) === 0)
+    }
+
+    const current = history[stepNumber];
+    const winner = calculateWinner(current.squares);
+
+    const moves = history.map((step, move) => {
+        const desc = move ?
+            'Go to move #' + move :
+            'Go to game start';
+        return (
+            <li key={move}>
+                <button onClick={() => jumpTo(move)}>{desc}</button>
+            </li>
+        );
+    });
+
+    let status;
+    if (winner) {
+        status = 'Winner: ' + winner;
+    } else {
+        status = 'Next player: ' + (xIsNext ? 'X' : 'O');
+    }
+
+    const handleClick = (i) => {
+        const hs = history.slice(0, stepNumber + 1);
+        
+        const current = hs[hs.length - 1];
+        const squares = current.squares.slice();
+        if (calculateWinner(squares) || squares[i]) {
+            return;
+        }
+        squares[i] = xIsNext ? 'X' : 'O';
+        setHistory(hs.concat([{
+            squares: squares,
+        }]));
+        setXIsNext(!xIsNext);
+        setStepNumber(hs.length);
+    }
+
     return (
         <div className="game">
             <div className="game-board">
-                <Board />
+                <Board
+                    squares={current.squares}
+                    onClick={(i) => handleClick(i)}
+                />
             </div>
             <div className="game-info">
-                <div>{/* status */}</div>
-                <ol>{/* TODO */}</ol>
+                <div>{status}</div>
+                <ol>{moves}</ol>
             </div>
         </div>
     );
